a783fef15e5de25545a3c973dc253bb1f0d0761f
Ineseb <inesebuda@inbox.lv>
2023-03-06 16:43:49 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 3;
+        x = x * 17;
+        System.out.println(x + x);
+
+        double value = 0.4;
+        for (int i = 0; i < 100; i++) {
+            value += 0.2;
+        }
+        System.out.println(value);
+
+    }
+}
