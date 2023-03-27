15940be350cf467cb03b301da9ac14036c0a149c
Ineseb <inesebuda@inbox.lv>
2023-03-27 15:45:52 +0300

    got numbers from user till user get 0

---
+++ src/ExerciseEight.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ExerciseEight {
+    /*
+    Exercise 8
+    Write a program that takes numbers from the user as long as the
+    number 0 is not given. When the number 0 is given, the program
+    calculates the sum of the numbers given and prints it in the console.
+    For example, for a series of given numbers: 3, 2, 5, 1, 0, the program
+    should write the number 11 in the console.   Get the data from the user
+    in the console using the Scanner class.
+
+     */
+    public static void main(String[] args) {
+Scanner inputReader = new Scanner(System.in);
+        System.out.println("Play game with me!");
+        int userNumber;
+
+        do{
+            System.out.println("Please write the number from 1 to 50");
+            userNumber=inputReader.nextInt();
+        }while(userNumber!=0);
+        System.out.println("Your given nuber sum is:");
+    }
+}
+
