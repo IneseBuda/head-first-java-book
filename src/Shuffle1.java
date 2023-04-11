8f08b8ac4133f39dfc8c85e57b17bcc6ee8c793d
Ineseb <inesebuda@inbox.lv>
2023-04-11 23:06:12 +0300

    while if scrampble, correct answer

--- src/Shuffle1.java
+++ src/Shuffle1.java
@@ -6,6 +6,9 @@ public class Shuffle1 {
                 System.out.print("a");
 
             }
+            x = x - 1;
+            System.out.print("-");
+
             if (x == 2) {
                 System.out.print("b c");
 
@@ -14,8 +17,6 @@ public class Shuffle1 {
                 System.out.print("d");
                 x = x - 1;
             }
-            x = x - 1;
-            System.out.print("-");
 
         }
     }
